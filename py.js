c57a1bd0db3d0576588403b7ee5df2f2261eae20
Mart Anthony Salazar <martsalazar0@gmail.com>
2023-02-11 14:23:02 +0800

    Add string functions

---
+++ py.js
@@ -0,0 +1,10 @@
+const Python = require("./python");
+new Python();
+
+name = "mart".capitalize();
+print("Hello python".count("hel"));
+print(name);
+print(name.find("art"));
+print(name.isLower());
+print("this is a python script".title());
+print("  JsPy  ".strip());
