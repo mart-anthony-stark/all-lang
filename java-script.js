0c75f96d0c740d8e860512d5e3d83a9238f63299
Mart Anthony Salazar <martsalazar0@gmail.com>
2023-02-11 14:22:59 +0800

    Add string functions

--- java-script.js
+++ java-script.js
@@ -1,3 +1,9 @@
-const { System } = require("./java");
+const Java = require("./java");
+new Java();
+
+const name = "Mart";
 
 System.out.println("Hello Java");
+System.out.println(name.equals("mart"));
+System.out.println(name.equalsIgnoreCase("mart"));
+System.out.println(name.contains("art"));
