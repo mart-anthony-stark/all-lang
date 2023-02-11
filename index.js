6067c47600da177ea629e0aab0cbdd3bec79e8b8
Mart Anthony Salazar <martsalazar0@gmail.com>
2023-02-11 12:57:24 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { print } = require("./python");
+const { System } = require("./java");
+const { Console } = require("./cs");
+const { printf } = require("./c");
+const { echo } = require("./php");
+const { println } = require("./rust");
+const { disp } = require("./matlab");
+
+print("Hello Python");
+System.out.println("Hello Java");
+printf("Hello C");
+Console.Writeline("Hello C#");
+echo("Hello PHP");
+println("Hello Rust");
+disp("Hello Matlab");
